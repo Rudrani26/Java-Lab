0802ec7a056dd25f0e9a171753725b33a030c264
Rudrani26 <66739928+Rudrani26@users.noreply.github.com>
2022-11-07 00:03:48 +0530

    Operations

---
+++ letscalc/Operations.java
@@ -0,0 +1,29 @@
+package letscalc;
+
+public class Operations
+{
+	public void add(int a, int b)
+	{
+		int z;
+		z = a+b;
+		System.out.println("Answer is "+ z);
+	}
+	public void sub(int a, int b)
+	{
+		int z;
+		z = a-b;
+		System.out.println("Answer is "+ z);
+	}
+	public void mult(int a, int b)
+	{
+		int z;
+		z = a*b;
+		System.out.println("Answer is "+ z);
+	}
+	public void div(int a, int b)
+	{
+		int z;
+		z = a/b;
+		System.out.println("Answer is "+ z);
+	}
+}
